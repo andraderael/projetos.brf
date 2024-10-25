fd96af907cd0d0504df67daf61625906f4befede
Israel Andrade <raelcordeiro2@gmail.com>
2024-10-25 18:18:45 -0300

    estudos

--- teste.py
+++
@@ -1,7 +0,0 @@
-preco = float(input('Digite o preço do produto: '))
-percentual = float(input('Digite o percentual de desconto: (0-100%): '))
-desconto = preco * (percentual / 100)
-final = preco - desconto 
-
-print(f'O preço do produto é {preco}. Desconto de {percentual}%')
-print(f'Valor calculado de desconto: {desconto}. Valor final do produto: {final}')
